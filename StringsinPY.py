da01c3be53c27fa37d81466da6f31176aec04b63
Godugu Veena <goduguveena@gmail.com>
2024-12-26 20:22:10 +0530

    Add files via upload

---
+++ StringsinPY.py
@@ -0,0 +1,10 @@
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(a)
+for x in "banana":
+  print(x)
+
+txt = "The best things in life are free!"
+print("free" in txt)
