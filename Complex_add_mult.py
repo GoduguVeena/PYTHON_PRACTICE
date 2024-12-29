96d9919b32ed11a321fc43fcf3c4068094578385
Godugu Veena <goduguveena@gmail.com>
2024-12-29 21:15:52 +0530

    Add files via upload

---
+++ Complex_add_mult.py
@@ -0,0 +1,8 @@
+a=int(input("enter real part 1st number:"))
+b=int(input("enter imaginary part of 1st number:"))
+c=int(input("enter real part of 2nd number:"))
+d=int(input("enter imaginary part of 2nd number:"))
+z1=complex(a,b)
+z2=complex(c,d)
+print("addition of complex numbers:",(z1+z2))
+print("product of complex numbers:",(z1*z2))
