a6864342c9b074d5a7cb77067bd14aab883b3709
Godugu Veena <goduguveena@gmail.com>
2025-01-28 23:24:42 +0530

    Delete Bio.py

--- Bio.py
+++
@@ -1,4 +0,0 @@
-print("MY NAME IS VEENA")
-print("I am pursuing my B.Tech in Computer Science and Engineering with Specialization in DataScience")
-print("I am a Content Creator at NEXUS Swarm")
-print("I am a Campus Ambassador at ALEPH ZERO INDIA")
