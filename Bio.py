405507360b0233b2d328ce2a517f873de0daf207
Godugu Veena <goduguveena@gmail.com>
2025-01-28 23:22:41 +0530

    Add files via upload

---
+++ Bio.py
@@ -0,0 +1,4 @@
+print("MY NAME IS VEENA")
+print("I am pursuing my B.Tech in Computer Science and Engineering with Specialization in DataScience")
+print("I am a Content Creator at NEXUS Swarm")
+print("I am a Campus Ambassador at ALEPH ZERO INDIA")
